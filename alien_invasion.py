a1faf7c55b0303eeec6b44e5171ac0b209854200
btbrad <biantaobrad@gmail.com>
2025-04-24 21:38:44 +0800

    点击play，开始游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -61,7 +61,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-               self._check_keyup_events(event)    
+               self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)       
+
+    def _check_play_button(self, mouse_pos):
+        """在玩家单击Play按钮时开始新游戏"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.game_active = True
 
     def _check_keydown_events(self, event):
         """响应按下"""
