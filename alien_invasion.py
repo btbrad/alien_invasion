db354a7266194e48ede70888e495d103636359b5
btbrad <biantaobrad@gmail.com>
2025-04-25 10:55:02 +0800

    更新得分

--- alien_invasion.py
+++ alien_invasion.py
@@ -129,6 +129,11 @@ class AlienInvasion:
         """响应子弹和外星人的碰撞"""
         # 删除发生碰撞的子弹和外星人
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+
         if not self.aliens:
             # 删除现有的子弹并创建一个新的外星舰队
             self.bullets.empty()
